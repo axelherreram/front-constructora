edea902210b0a504115303ae92201d9e2eea2944
axelPrac <axelherreram7@gmail.com>
2024-01-03 10:49:59 -0600

    configs

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { HashRouter as BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Login from "./components/Login";
 import AdminPage from "./pages/AdminPage";
 import PageFYV from "./pages/PageFYV";

a663e573b18ea07d61988991e36ef77ec3cb80ac
axelPrac <axelherreram7@gmail.com>
2024-01-03 12:04:53 -0600

    nog v3

--- src/pages/PageProyectos.jsx
+++ src/pages/PageProyectos.jsx
@@ -105,6 +105,7 @@ const PageProyectos = (props) => {
         date: fechaProyecto,
         munici_id: Muni_id,
       });
+      console.log("Respuesta completa del servidor:", response);
 
       if (response.status === 201) {
         console.log("Proyecto creado exitosamente");
@@ -124,7 +125,6 @@ const PageProyectos = (props) => {
         setFechaProyecto("");
         setShowModal(false);
       } else {
-      console.log("Respuesta completa del servidor:", response);
 
         // Manejar el error 400
         if (response.status === 400) {

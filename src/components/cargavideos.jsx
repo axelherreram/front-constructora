530b92760f6df7ca7d158f3c1c65c75bc3db744f
FlyBenja <aldanabenjamin185@gmail.com>
2024-01-10 14:08:21 -0600

    Cambios Videos

--- src/components/cargavideos.jsx
+++ src/components/cargavideos.jsx
@@ -105,32 +105,7 @@ const ComponenteA = ({ proyectoID, updateCounter1, role }) => {
                 </Dropdown.Menu>
               </Dropdown>
             )}
-            <OverlayTrigger
-              show={tooltipVisible[pkP.id]}
-              placement="top"
-              overlay={
-                <Tooltip id={`tooltip-${pkP.id}`}>
-                  {pkP.name}
-                </Tooltip>
-              }
-            >
-              <img
-                src={info}
-                alt="Información"
-                style={{
-                  width: "25px",
-                  height: "25px",
-                  cursor: "pointer",
-                  position: "absolute",
-                  bottom: "263px",
-                  right: "293px",
-                }}
-                onClick={(e) => {
-                  e.stopPropagation(); // Evitar que el clic en el ícono cierre el tooltip
-                  toggleTooltip(pkP.id);
-                }}
-              />
-            </OverlayTrigger>
+            
           </div>
         ))
       )}
